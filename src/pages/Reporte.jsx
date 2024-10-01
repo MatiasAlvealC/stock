fe296c30fe0db21f3347f55ba1926c79f18e9ff1
Benja <munozburgosbenjamin@gmail.com>
2024-09-30 23:00:51 -0300

    avance reporte

--- src/pages/Reporte.jsx
+++ src/pages/Reporte.jsx
@@ -1,8 +1,12 @@
+import '../assets/styles/report.css';
+import paginaconstr from '../assets/paginaconstr.jpg';
+
 function Reporte() {
   return (
     <div>
       <h1>Reporte</h1>
-      <p>Aquí se generarán los reportes del inventario.</p>
+      <p>Ingenieros Trabajando.</p>
+      <img src={paginaconstr} alt="Reporte" />
     </div>
   );
 }
